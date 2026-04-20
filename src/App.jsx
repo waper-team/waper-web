f76120d031bea914569746d5953465050ba14368
Said Quioto <said.quioto@uap.edu.ar>
2026-04-19 21:31:36 -0300

    chore: eliminando referencias hacia el archivo prueba

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import './App.css'
-import Prueba from './components/Prueba'
 
 function App() {
   return (
     <>
       <h1>Waper</h1>
       <p>Acá inicia la aplicación</p>
-      <Prueba/>
     </>
   )
 }
